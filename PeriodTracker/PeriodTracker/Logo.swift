aa119cd3dd88822113eb6771791678bdb23905d8
Mahdiar <mahdiar@Mahdiars-MacBook-Pro.local>
2017-06-21 16:43:31 +0430

    Splash screen completed

---
+++ PeriodTracker/PeriodTracker/Logo.swift
@@ -0,0 +1,18 @@
+//
+//  Logo.swift
+//  PeriodTracker
+//
+//  Created by Mahdiar  on 6/21/17.
+//  Copyright © 2017 Mahdiar . All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class Logo: UIView {
+
+    override func draw(_ rect: CGRect) {
+        StyleKitName.drawCanvas1()
+    }
+
+}
