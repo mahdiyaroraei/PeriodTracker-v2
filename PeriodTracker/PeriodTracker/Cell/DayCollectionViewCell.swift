c286c23e4c8b3334f0692033c6a151b87bf8a4f9
Mahdiar <mahdiar@Mahdiars-MacBook-Pro.local>
2017-06-24 19:36:02 +0430

    Calendar view

---
+++ PeriodTracker/PeriodTracker/Cell/DayCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DayCollectionViewCell.swift
+//  PeriodTracker
+//
+//  Created by Mahdiar  on 6/24/17.
+//  Copyright © 2017 Mahdiar . All rights reserved.
+//
+
+import UIKit
+
+class DayCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ PeriodTracker/PeriodTracker/Cell/MonthTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MonthTableViewCell.swift
+//  PeriodTracker
+//
+//  Created by Mahdiar  on 6/24/17.
+//  Copyright © 2017 Mahdiar . All rights reserved.
+//
+
+import UIKit
+
+class MonthTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
