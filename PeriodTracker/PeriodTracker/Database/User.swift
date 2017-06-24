6ed076dc82f8fdbbadbd6a9b7234c969487e4cdd
Mahdiar <mahdiar@Mahdiars-MacBook-Pro.local>
2017-06-24 13:44:11 +0430

    Pending files commit

---
+++ PeriodTracker/PeriodTracker/Database/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  PeriodTracker
+//
+//  Created by Mahdiar  on 6/22/17.
+//  Copyright © 2017 Mahdiar . All rights reserved.
+//
+
+import RealmSwift
+
+class User: Object {
+
+    dynamic var user_id = 0
+    dynamic var email = ""
+    dynamic var license_id = 0
+}
