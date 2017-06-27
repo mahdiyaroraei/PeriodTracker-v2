17696c89d8eb0dbc2f364e94d7ec61f99394b48b
Mahdiar <mahdiar@Mahdiars-MacBook-Pro.local>
2017-06-27 18:07:59 +0430

    Pods update CalendarViewController basically completed

--- PeriodTracker/PeriodTracker/Controllers/CalendarViewController.swift
+++ PeriodTracker/PeriodTracker/Controllers/CalendarViewController.swift
@@ -8,19 +8,29 @@
 
 import UIKit
 
-class CalendarViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+class CalendarViewController: UIViewController , UITableViewDelegate , UITableViewDataSource , SelectCellDelegate {
     @IBOutlet weak var monthsTableView: UITableView!
     
+    @IBOutlet weak var stackView: UIStackView!
     let cellReuseIdentifier = "cell"
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var todayLabel: UILabel!
     
     // start of day and month
     let nowDate = Calendar.current.startOfDay(for: Date())
+    
+    static var selectedDate: Date?
 
+    @IBAction func goToToday(_ sender: Any) {
+        monthsTableView.scrollToRow(at: IndexPath(row: 25, section: 0), at: .middle, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // This view controller itself will provide the delegate methods and row data for the table view.
+        todayLabel.text = "\(String(describing: Calendar(identifier: .persian).dateComponents([.day], from: nowDate).day!))"
+        
         monthsTableView.delegate = self
         monthsTableView.dataSource = self
         
@@ -35,6 +45,20 @@ class CalendarViewController: UIViewController , UITableViewDelegate , UITableVi
         // Hide scrollbar
         monthsTableView.showsHorizontalScrollIndicator = false
         monthsTableView.showsVerticalScrollIndicator = false
+        
+        // Hide seperator line between cells
+        monthsTableView.separatorStyle = .none
+        
+        // Add top and bottom border for stackView
+        let topLineLayer = CALayer()
+        topLineLayer.backgroundColor = Colors.normalCellColor.cgColor
+        topLineLayer.frame = CGRect(x:-50,y: 0, width:stackView.frame.size.width + 100, height:1)
+        stackView.layer.addSublayer(topLineLayer)
+        
+        let bottomLineLayer = CALayer()
+        bottomLineLayer.backgroundColor = Colors.normalCellColor.cgColor
+        bottomLineLayer.frame = CGRect(x:-50, y:stackView.frame.size.height - 1, width:stackView.frame.size.width + 100, height:1)
+        stackView.layer.addSublayer(bottomLineLayer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,15 +78,30 @@ class CalendarViewController: UIViewController , UITableViewDelegate , UITableVi
         // for hide highlight
         cell.selectionStyle = .none
         
+        // for update tableview after selecting cell from collectionview
+        cell.delegate = self
+        
         cell.monthDate = Calendar.current.date(byAdding: .month, value: indexPath.row - 25 , to: nowDate)
         cell.refresh()
         
         return cell
     }
     
+    func updateTableView() {
+        self.monthsTableView.reloadData()
+    }
+    
+    func cannotSelectFuture() {
+        showToast(message: "نمی‌توانید روز های آینده را انتخاب کنید!")
+    }
+    
+    func changeTitle(title: String) {
+        titleLabel.text = title
+    }
+    
     // return tableview cell height for no additional padding
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let tableViewCellHeight = monthsTableView.frame.size.width + 50
+        let tableViewCellHeight = monthsTableView.frame.size.width + 10
         
         return tableViewCellHeight
     }
@@ -71,4 +110,24 @@ class CalendarViewController: UIViewController , UITableViewDelegate , UITableVi
         print("tap!")
     }
 
+}
+extension UIViewController{
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 110, y: self.view.frame.size.height-100, width: 220, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center;
+        toastLabel.font = UIFont(name: "IRANSans(FaNum)", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 1, delay: 0.8, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
 }
---
+++ PeriodTracker/PeriodTracker/Utilities/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  PeriodTracker
+//
+//  Created by Mahdiar  on 6/27/17.
+//  Copyright © 2017 Mahdiar . All rights reserved.
+//
+
+import UIKit
+
+class Colors: NSObject {
+    static let accentColor = Utility.uicolorFromHex(rgbValue: 0x7CA113)
+    static let normalCellColor = Utility.uicolorFromHex(rgbValue: 0xe1e4e7)
+    static let normalCellTextColor = Utility.uicolorFromHex(rgbValue: 0x36454A)
+
+}
